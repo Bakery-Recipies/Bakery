b394272ed578f4ce962f67e99613d957db96f816
ayahrimawi <ayaalrimawi406@gmail.com>
2024-09-03 10:39:29 +0300

    serverChanges

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //imports
 const express = require("express");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const mongoose = require("./config/dbConfig");
@@ -11,12 +11,13 @@ const dishRoutesAya = require("./routes/dishRoutesAya");
 //server variables
 const port = process.env.PORT || 3000;
 const app = express();
-
+const corsConfig = {
+  credentials: true,
+};
 //server middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors(corsConfig));
+app.use(bodyParser.json());
 app.use(cookieParser());
-
 
 // Use the recipie routes
 app.use("/api", recipieRoutesAya);
